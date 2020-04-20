bff259d5fc4a11b9d6c7c51e481b575f4ba7efa4
Obaitan <elero.obaitan@gmail.com>
2020-04-20 22:49:04 +0100

    Finished draft Lumos website template.

--- js/active.js
+++ js/active.js
@@ -1,43 +1,25 @@
-// for smooth scrolling to page sections
-// $(function () {
-//     $('a[href*="#"]:not([href="#"])').click(function () {
-//         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//             var target = $(this.hash);
-//             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//             if (target.length) {
-//                 $('html, body').animate({
-//                     scrollTop: target.offset().top
-//                 }, 1750);
-//                 return false;
-//             }
-//         }
-//     });
-// });
+//Get the scroll-to-top button
+var mybutton = document.getElementById("top-btn");
 
+// When the user scrolls down 450px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
 
-// To open and close sidebar
-let sideBarOpen = false;
-function openNav() {
-    sideBarOpen = true;
-    document.getElementById("nav").style.width = "275px";
+function scrollFunction() {
+    if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
-function closeNav() {
-    sideBarOpen = false;
-    document.getElementById("nav").style.width = "0px";
-}
+// // Menu Tracking
+// $(document).ready(function () {
 
-// Closing sidebar by menu item clicks and by clicking outside sidebar
-// $(function () {
-//     $(".nav-link").on('click', function (event) {
-//         event.stopPropagation();
-//         event.stopImmediatePropagation();
-//         closeNav();
+//     $(".nav-link").click(function () {
+//         $(".nav-link").removeClass("active");
+//         $(this).addClass("active");
 //     });
 
-//     $(".main").on('click', function (event) {
-//         closeNav();
-//     });
 // });
 
 
